c663aac211e3b02938f0c05bec921f461576b9ef
Jorge Matos <jisapjisap@gmail.com>
2024-07-11 19:27:52 +0200

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "./lib/utils";
+import { ThemeProvider } from "next-themes";
 
 
 const fontSans = FontSans({
@@ -30,7 +31,9 @@ export default function RootLayout({
         fontSans.variable
       )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
